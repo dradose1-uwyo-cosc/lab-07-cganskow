d510f6665e3cb0c776df867163d7340d3899039a
Caitlyn Ganskow <cganskow@uwyo.edu>
2024-10-31 22:02:27 +0000

    Final

--- Ganskow-COSC1010-lab07.py
+++ Ganskow-COSC1010-lab07.py
@@ -1,12 +1,10 @@
-# Your Name Here
+# Caitlyn Ganskow
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
+# 10-31-24
+# Lab 07
+# Lab Section: 12
 # Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# https://www.w3schools.com/python/ref_string_replace.asp to learn how to replace a string
 
 
 # Prompt the user for an upper bound 
@@ -18,8 +16,21 @@
 # You will continue to prompt the user until a proper integer value is entered
 
 factorial = 1
+x = 1
 
-print(f"The result of the factorial based on the given bound is {factorial}")
+while True:
+    upper_bound = input("Please provide a positive whole number")
+    if upper_bound.isnumeric() and int(upper_bound) > 0:
+        while x <= int(upper_bound):
+            factorial = factorial * x
+            x += 1
+        print(f"The result of the factorial based on the given bound is {factorial}")
+        break
+    else:
+        print(f"You provided {upper_bound} which is not a positive whole number.")
+
+
+
 
 print("*"*75)
 # Create a while loop that prompts a user for input of an integer values
@@ -38,6 +49,21 @@ print("*"*75)
 # The sum should start at 0 
 
 num_sum = 0 
+
+while True:
+    integer = input("Please provide an integer (eg. 5, -9, 0), or say 'exit' to quit: ")
+    if "-" in integer:
+        neg_integer = integer.replace("-", "")
+        if neg_integer.isnumeric():
+            num_sum -= int(neg_integer)
+        else:
+            print(f"{integer} is not a valid negative number")
+    elif integer.isnumeric():
+        num_sum += int(integer)
+    elif integer.lower() == "exit":
+        break
+    else:
+        print(f"{integer} is not a valid integer")
 
 print(f"Your final sum is {num_sum}")
 
@@ -59,4 +85,45 @@ print("*"*75)
 # Print the result of the equation
 # Again, loop through prompting the user for input until `exit` in any casing is input 
 
-        
+while True:
+    inp_equation = input("Please provide an equation to solve, type 'exit' to quit: ")
+    equation = inp_equation.replace(" ", "")
+    if '+' in equation:
+        nums = equation.split("+")
+        if nums[0].isnumeric() and nums[1].isnumeric():
+            solution = int(nums[0]) + int(nums[1])
+            print(f"{nums[0]} + {nums[1]} = {solution}")
+        else:
+            print(f"{equation} needs to be integers")
+    elif '-' in equation:
+        nums = equation.split("-")
+        if nums[0].isnumeric() and nums[1].isnumeric():
+            solution = int(nums[0]) - int(nums[1])
+            print(f"{nums[0]} - {nums[1]} = {solution}")
+        else:
+            print(f"{equation} needs to be integers")
+    elif '/' in equation:
+        nums = equation.split("/")
+        if nums[0].isnumeric() and nums[1].isnumeric():
+            solution = int(nums[0]) / int(nums[1])
+            print(f"{nums[0]} / {nums[1]} = {solution}")
+        else:
+            print(f"{equation} needs to be integers")
+    elif '*' in equation:
+        nums = equation.split("*")
+        if nums[0].isnumeric() and nums[1].isnumeric():
+            solution = int(nums[0]) * int(nums[1])
+            print(f"{nums[0]} * {nums[1]} = {solution}")
+        else:
+            print(f"{equation} needs to be integers")
+    elif '%' in equation:
+        nums = equation.split("%")
+        if nums[0].isnumeric() and nums[1].isnumeric():
+            solution = int(nums[0]) % int(nums[1])
+            print(f"{nums[0]} % {nums[1]} = {solution}")
+        else:
+            print(f"{equation} needs to be integers")
+    elif inp_equation.lower() == 'exit':
+        break
+    else:
+        print("Invalid input")
